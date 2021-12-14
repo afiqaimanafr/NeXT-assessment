394d5ac9c0e94d7d17c425a29cdbb90940a17ae3
afiqaimanafr <aaafr98@gmail.com>
2021-12-14 15:58:26 +0800

    update: listtile

--- lib/screens/iss_location_screen.dart
+++ lib/screens/iss_location_screen.dart
@@ -24,7 +24,19 @@ class _ISSLocationScreenState extends State<ISSLocationScreen> {
       date12,
       date13;
 
-  var timeStamp7;
+  var timeStamp1,
+      timeStamp2,
+      timeStamp3,
+      timeStamp4,
+      timeStamp5,
+      timeStamp6,
+      timeStamp7,
+      timeStamp8,
+      timeStamp9,
+      timeStamp10,
+      timeStamp11,
+      timeStamp12,
+      timeStamp13;
   @override
   void initState() {
     date1 = widget.formattedDate.subtract(
@@ -32,31 +44,37 @@ class _ISSLocationScreenState extends State<ISSLocationScreen> {
         minutes: 60,
       ),
     );
+    timeStamp1 = date1.millisecondsSinceEpoch ~/ 1000;
     date2 = widget.formattedDate.subtract(
       const Duration(
         minutes: 50,
       ),
     );
+    timeStamp2 = date2.millisecondsSinceEpoch ~/ 1000;
     date3 = widget.formattedDate.subtract(
       const Duration(
         minutes: 40,
       ),
     );
+    timeStamp3 = date3.millisecondsSinceEpoch ~/ 1000;
     date4 = widget.formattedDate.subtract(
       const Duration(
         minutes: 30,
       ),
     );
+    timeStamp4 = date4.millisecondsSinceEpoch ~/ 1000;
     date5 = widget.formattedDate.subtract(
       const Duration(
         minutes: 20,
       ),
     );
+    timeStamp5 = date5.millisecondsSinceEpoch ~/ 1000;
     date6 = widget.formattedDate.subtract(
       const Duration(
         minutes: 10,
       ),
     );
+    timeStamp6 = date6.millisecondsSinceEpoch ~/ 1000;
     date7 = widget.formattedDate.subtract(
       const Duration(
         minutes: 0,
@@ -68,49 +86,408 @@ class _ISSLocationScreenState extends State<ISSLocationScreen> {
         minutes: 10,
       ),
     );
+    timeStamp8 = date8.millisecondsSinceEpoch ~/ 1000;
     date9 = widget.formattedDate.add(
       const Duration(
         minutes: 20,
       ),
     );
+    timeStamp9 = date9.millisecondsSinceEpoch ~/ 1000;
     date10 = widget.formattedDate.add(
       const Duration(
         minutes: 30,
       ),
     );
+    timeStamp10 = date10.millisecondsSinceEpoch ~/ 1000;
     date11 = widget.formattedDate.add(
       const Duration(
         minutes: 40,
       ),
     );
+    timeStamp11 = date11.millisecondsSinceEpoch ~/ 1000;
     date12 = widget.formattedDate.add(
       const Duration(
         minutes: 50,
       ),
     );
+    timeStamp12 = date12.millisecondsSinceEpoch ~/ 1000;
     date13 = widget.formattedDate.add(
       const Duration(
         minutes: 60,
       ),
     );
+    timeStamp13 = date13.millisecondsSinceEpoch ~/ 1000;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'ISS Location',
-          style: TextStyle(
-            fontFamily: primaryFamilyFont,
-            fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: const Text(
+            'DateTime and Timestamp',
+            style: TextStyle(
+              fontFamily: primaryFamilyFont,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      body: Text(
-        timeStamp7.toString(),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date1.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp1.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date2.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp2.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date3.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp3.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date4.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp4.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date5.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp5.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date6.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp6.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date7.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp7.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date8.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp8.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date9.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp9.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date10.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp10.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date11.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp11.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date12.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp12.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 8,
+                ),
+                child: Card(
+                  color: kPrimaryColor,
+                  child: ListTile(
+                    title: Text(
+                      'DateTime: ${date13.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Timestamp: ${timeStamp13.toString()}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: kPrimaryDarkColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
