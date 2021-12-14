f3e1fdfb28449939401c5bf47e5c6af50daba580
afiqaimanafr <aaafr98@gmail.com>
2021-12-14 10:11:12 +0800

    intl update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ISS NeXT Assessment',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
       home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
