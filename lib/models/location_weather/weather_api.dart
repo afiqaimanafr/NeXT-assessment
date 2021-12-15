81d4658cde2923400ba0a20408c5b5a32234eb49
afiqaimanafr <aaafr98@gmail.com>
2021-12-15 14:51:07 +0800

    weather

--- lib/models/location_weather/weather_api.dart
+++ lib/models/location_weather/weather_api.dart
@@ -5,15 +5,12 @@ import 'package:iss_next_assessment/models/location_weather/weather_model.dart';
 
 class WeatherApi {
   static Future<List<WeatherModel>> getISSWeather(lat, long, timestamp) async {
-    final issWeatherURL =
-        'https://history.openweathermap.org/data/3.0/history/timemachine?lat=' +
-            lat +
-            '&lon=' +
-            long +
-            '&dt=' +
-            timestamp +
-            '&appid=' +
-            apiKey;
+    final issWeatherURL = 'api.openweathermap.org/data/2.5/weather?lat=' +
+        lat +
+        '&lon=' +
+        long +
+        '&appid=' +
+        apiKey;
 
     final response = await http.get(
       Uri.parse(
